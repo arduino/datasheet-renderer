94de1ee73c7031a871d7696085867c4f219e45e8
Sebastian Romero <s.romero@arduino.cc>
2022-07-08 15:43:10 +0200

    Use scoped import

--- logic/datasheet.js
+++ logic/datasheet.js
@@ -2,7 +2,7 @@ import { existsSync, readFileSync, statSync } from 'fs';
 import { load } from 'js-yaml';
 import fm from 'front-matter';
 import findParentDir from 'find-parent-dir';
-import marked from 'marked';
+import { marked } from 'marked';
 import * as path from 'path';
 
 export class Datasheet {
