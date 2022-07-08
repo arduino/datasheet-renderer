a8fb6627419dac13c6168388f0cd6442fd0e9123
Sebastian Romero <s.romero@arduino.cc>
2022-07-08 16:02:06 +0200

    Add binary command

---
+++ app.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { DatasheetRenderer } from './logic/datasheet-renderer.js';
+
+(async function main() {
+    const args = process.argv.slice(2)
+    const datasheetsSourcePath = args[1];
+    const configFilePath = args[0];
+    const renderer = new DatasheetRenderer(configFilePath, datasheetsSourcePath);
+    
+    const generatedDatasheets = (await renderer.generatePDFsFromMarkdownFiles()).length;
+    const allDatasheetsGenerated = generatedDatasheets == renderer.datasheets.length;
+    const failedDatasheets = renderer.datasheets.length - generatedDatasheets;
+    
+    if(generatedDatasheets > 0)
+        console.log("✅ %s Datasheets generated.", generatedDatasheets);
+    if(failedDatasheets > 0)
+        console.log("❌ %s Datasheets couldn't be generated.", failedDatasheets);
+    process.exit(allDatasheetsGenerated ? 0 : -1);
+})()
